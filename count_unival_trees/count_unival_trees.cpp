954aacc498b94e8dcb9ffd2f5f49cbde50e0102a
Samuel Luo <samuel.richard.luo@gmail.com>
2018-06-20 13:05:00 -0400

    Improved count_unival_trees in C++.

--- count_unival_trees/count_unival_trees.cpp
+++ count_unival_trees/count_unival_trees.cpp
@@ -1,8 +1,9 @@
-// g++ -std=c++14 count_unival_trees.cpp
+// g++ -std=c++17 count_unival_trees.cpp
 #include <iostream>
 
 // ----------------------------------------
 struct Node {
+    // Forward declarations can only be defined as pointers
     char val;
     Node* left;
     Node* right;
@@ -40,14 +41,39 @@ int count_unival_trees(Node* root) {
     return num_unival;
 }
 // ----------------------------------------
+std::pair<int, bool> count_unival_trees_2_helper(Node* root) {
+    if (root == NULL) return std::pair<int, bool>(0, true);
+    int num_unival = 0;
+    auto[num_left, left_unival]   = count_unival_trees_2_helper(root->left);
+    auto[num_right, right_unival] = count_unival_trees_2_helper(root->right);
+    num_unival = num_left + num_right;
+    if ((left_unival && right_unival) &&
+        (root->left == NULL || root->val == root->left->val) &&
+        (root->right == NULL || root->val == root->right->val)) {
+        return std::pair<int, bool>(num_unival + 1, true);
+    } else {
+        return std::pair<int, bool>(num_unival, false);
+    }
+}
+int count_unival_trees_2(Node* root) {
+    auto[count, is_unival] = count_unival_trees_2_helper(root);
+    return count;
+}
+
+// ----------------------------------------
 int main() {
+    // works: new Node, new Node();
+    // does not work: new Node('a'); no constructor & not type Node*
+    // root_node = new Node; Node* root = &root;
     Node* root = newNode('a');
     root->left = newNode('a');
     root->right = newNode('a');
     bool result = is_unival(root);
     if (result) std::cout << "Is a unival tree" << std::endl;
-    else std::cout << "Not a unival tree" << std::endl;    
+    else std::cout << "Not a unival tree" << std::endl;
     int count = count_unival_trees(root);
+    std::cout << count << std::endl;
+    count = count_unival_trees_2(root);
     std::cout << count << std::endl;
 
     root = newNode('a');
@@ -59,6 +85,8 @@ int main() {
     if (result) std::cout << "Is a unival tree" << std::endl;
     else std::cout << "Not a unival tree" << std::endl;
     count = count_unival_trees(root);
+    std::cout << count << std::endl;
+    count = count_unival_trees_2(root);
     std::cout << count << std::endl;
 
     root = newNode('a');
@@ -72,6 +100,8 @@ int main() {
     else std::cout << "Not a unival tree" << std::endl;
     count = count_unival_trees(root);
     std::cout << count << std::endl;
+    count = count_unival_trees_2(root);
+    std::cout << count << std::endl;
 
     root = newNode('b');
     root->right = newNode('b');
@@ -79,5 +109,7 @@ int main() {
     if (result) std::cout << "Is a unival tree" << std::endl;
     else std::cout << "Not a unival tree" << std::endl;
     count = count_unival_trees(root);
+    std::cout << count << std::endl;
+    count = count_unival_trees_2(root);
     std::cout << count << std::endl;
 }
