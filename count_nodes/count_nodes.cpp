9f6328d6417d5055fc5d64a29dfacb8e4115a9bd
Samuel Luo <samuel.richard.luo@gmail.com>
2018-06-23 19:32:06 -0400

    Added count_nodes in C++.

---
+++ count_nodes/count_nodes.cpp
@@ -0,0 +1,46 @@
+// g++ -std=c++17 count_nodes.cpp
+#include <iostream>
+#include <queue>
+
+struct Node {
+    int val = 0;
+    Node* left = NULL;
+    Node* right = NULL;
+};
+
+int count_nodes_1(Node* root) {
+    if (root == NULL)
+        return 0;
+    return 1 + count_nodes_1(root->left) + count_nodes_1(root->right);
+}
+
+int count_nodes_2(Node* root) {
+    std::queue<Node*> node_queue;
+    node_queue.push(root);
+    int count = 0;
+    while (!node_queue.empty()) {
+        Node* node = node_queue.front();
+        node_queue.pop();
+        count += 1;
+        if (node->left != NULL)
+            node_queue.push(node->left);
+        if (node->right != NULL)
+            node_queue.push(node->right);
+    }
+    return count;
+}
+
+int main() {
+    int result;
+    Node* root;
+    
+    root = new Node; root->val = 1;
+    root->left = new Node; root->left->val = 2;
+    root->right = new Node; root->right->val = 3;
+    root->left->left = new Node; root->left->left->val = 4;
+    root->left->right = new Node; root->left->right->val = 5;
+    result = count_nodes_1(root);
+    std::cout << result << std::endl;
+    result = count_nodes_2(root);
+    std::cout << result << std::endl;
+}
