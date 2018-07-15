3950f8915551faa9f5fa66780ac10b58a6786389
Samuel Luo <samuel.richard.luo@gmail.com>
2018-07-14 23:59:37 -0400

    Improved mult_2_div_3.

--- mult_2_div_3/mult_2_div_3.py
+++ mult_2_div_3/mult_2_div_3.py
@@ -5,12 +5,27 @@ Find the operations that would reach a target number.
 
 target = 7
 queue = [1]
-operations = []
+operations = {1: []}
 while len(queue) != 0:
     num = queue.pop(0)
+    print(num)
+    print(queue)
+    if num % 2 == 0 and num//2 in operations.keys():
+        operations[num] = operations[num//2] + [2]
+    elif num*3+0 in operations.keys():
+        operations[num] = operations[num*3+0] + [3]
+    elif num*3+1 in operations.keys():
+        operations[num] = operations[num*3+1] + [3]
+    elif num*3+2 in operations.keys():
+        operations[num] = operations[num*3+2] + [3]
     if num == target:
-        print('target found')
+        ops = operations[num]
+        for i in range(len(ops)):
+            print(i, end=' ')
+            if i == 2:
+                print('*', end=' ')
+        print(operations[num])
         break
     queue.append(num*2)
-    if num//2 != 0:
+    if num//3 != 0:
         queue.append(num//3)
