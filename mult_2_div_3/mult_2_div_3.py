ea45a47f89c7a7f733411f84b39468e90d5d7e6d
Samuel Luo <samuel.richard.luo@gmail.com>
2018-07-15 00:12:01 -0400

    Fixed mult_2_div_3 in Python.

--- mult_2_div_3/mult_2_div_3.py
+++ mult_2_div_3/mult_2_div_3.py
@@ -1,6 +1,6 @@
 """
-Suppose you start with the number 1 and can multiply (*) by 2 or divide (//) by 3.
-Find the operations that would reach a target number.
+Suppose you start with the number 1 and can multiply by 2 or floor divide by 3.
+Find the operations needed to reach a target number.
 """
 
 target = 7
@@ -8,8 +8,6 @@ queue = [1]
 operations = {1: []}
 while len(queue) != 0:
     num = queue.pop(0)
-    print(num)
-    print(queue)
     if num % 2 == 0 and num//2 in operations.keys():
         operations[num] = operations[num//2] + [2]
     elif num*3+0 in operations.keys():
@@ -20,11 +18,14 @@ while len(queue) != 0:
         operations[num] = operations[num*3+2] + [3]
     if num == target:
         ops = operations[num]
+        print('1', end='')
         for i in range(len(ops)):
-            print(i, end=' ')
-            if i == 2:
-                print('*', end=' ')
-        print(operations[num])
+            op = ops[i]
+            if op == 2:
+                print(' * 2', end='')
+            if op == 3:
+                print(' / 3', end='')
+        print(' = ' + str(target))
         break
     queue.append(num*2)
     if num//3 != 0:
