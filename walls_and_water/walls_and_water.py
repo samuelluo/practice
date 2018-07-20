4f4dfd8d08077bde4c6deaee60eb694b12f19eb7
Samuel Luo <samuel.richard.luo@gmail.com>
2018-07-20 00:34:56 -0400

    Fixed walls_and_water in Python.

--- walls_and_water/walls_and_water.py
+++ walls_and_water/walls_and_water.py
@@ -29,26 +29,23 @@ def walls_and_water_1(walls):
     print(water)
 
 def walls_and_water_2(walls):
-    """ O(3*n) = O(n) """
-    water = [0]
+    """ O(4*n) = O(n) """
     left = walls.copy()
-    right = walls.copy()
-
     for i in range(1, len(walls)):
         left[i] = max(left[i-1], left[i])
+    
+    right = walls.copy()
     for i in range(len(walls)-2, -1, -1):
         right[i] = max(right[i+1], right[i])
-
-    for i in range(len(walls)):
-        water.append(water[i] + min(left[i], right[i]))
-
-    print(walls)
-    print(left)
-    print(right)
-    print([min(left[i], right[i]) for i in range(len(left))])
+    
+    heights = [min(left[i], right[i]) for i in range(len(left))]
+    
+    water = [0]
+    for i in range(1, len(walls)):
+        water.append(water[i-1] + min(heights[i], heights[i-1]))
     print(water)
 
 # ---------------------------------------------------------
 walls = [1, 2, 1, 3, 1, 2, 3, 1]
-# walls_and_water_1(walls)
+walls_and_water_1(walls)
 walls_and_water_2(walls)
