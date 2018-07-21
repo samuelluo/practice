c8204372c51f75c71aec3878d888f1cfeb57c96a
Samuel Luo <samuel.richard.luo@gmail.com>
2018-07-21 19:01:11 -0400

    Edited balance_travel in Python.

--- balance_travel/balance_travel.py
+++ balance_travel/balance_travel.py
@@ -7,35 +7,43 @@ Split the candidates into two equal-sized groups while also minimizing travel
 cost.
 """
 
-costs = [(400, 200), (200, 100), (300, 150), (100, 50)]
-swap_costs = [(i[0]-i[1],) + i for i in costs]
-swap_costs = sorted(swap_costs, key=lambda x: x[0], reverse=True)
+# ---------------------------------------------------------
+def balance_travel(costs):
+    # Set up
+    swap_costs = [(i[0]-i[1],) + i for i in costs]
+    swap_costs = sorted(swap_costs, key=lambda x: x[0], reverse=True)
+    ny = []
+    sf = []
+    i = 0
 
-ny = []
-sf = []
-i = 0
+    # Assign the largest cost differences (opportunity cost) first
+    while len(ny) < len(costs)//2 and len(sf) < len(costs)//2:
+        cost = swap_costs[i]
+        if cost[0] > 0:    # If NY > SF, send to SF
+            sf.append(cost[1:])
+        else:              # If NY < SF, send to NY
+            ny.append(cost[1:])
+        i += 1
 
-# Assign the largest cost differences (opportunity cost) first
-while len(ny) < len(costs)//2 and len(sf) < len(costs)//2:
-    cost = swap_costs[i]
-    if cost[0] > 0:    # If NY > SF, send to SF
-        sf.append(cost[1:])
-    else:              # If NY < SF, send to NY
+    while len(ny) < len(costs)//2:
+        cost = swap_costs[i]
         ny.append(cost[1:])
-    i += 1
+        i += 1
 
-while len(ny) < len(costs)//2:
-    cost = swap_costs[i]
-    ny.append(cost[1:])
-    i += 1
+    while len(sf) < len(costs)//2:
+        cost = swap_costs[i]
+        sf.append(cost[1:])
+        i += 1
 
-while len(sf) < len(costs)//2:
-    cost = swap_costs[i]
-    sf.append(cost[1:])
-    i += 1
+    total_cost = sum([i[0] for i in ny]) + sum([i[1] for i in sf])
+    print("ny: " + str(ny))
+    print("sf: " + str(sf))
+    print("total cost: " + str(total_cost))
+    print()
 
-total_cost = sum([i[0] for i in ny]) + sum([i[1] for i in sf])
-print(swap_costs)
-print(ny)
-print(sf)
-print(total_cost)
+# ---------------------------------------------------------
+costs = [(400, 200), (200, 100), (300, 150), (100, 50)]
+balance_travel(costs)
+
+costs = [(1000, 1001), (1, 11)]
+balance_travel(costs)
