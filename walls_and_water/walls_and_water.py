fdc2cbbfefb204dc29c2c3c9093f2a1ef0472c80
Samuel Luo <samuel.richard.luo@gmail.com>
2018-07-21 22:49:33 -0400

    Added to walls_and_water in Python.

--- walls_and_water/walls_and_water.py
+++ walls_and_water/walls_and_water.py
@@ -5,11 +5,17 @@ each bar is 1, compute how much water it is able to trap after raining.
 For example, the following set of walls would be represented by the array of 
 numbers below:
 
+[1, 2, 1, 3, 1, 2, 3, 1]
+
       |     |
   |   |   | |
 |_|_|_|_|_|_|_|
 
-[1, 2, 1, 3, 1, 2, 3, 1]
+or: 
+
+   |  |
+ | | ||
+||||||||
 """
 
 # ---------------------------------------------------------
@@ -45,7 +51,55 @@ def walls_and_water_2(walls):
         water.append(water[i-1] + min(heights[i], heights[i-1]))
     print(water)
 
+def walls_and_water_3(walls):
+    """ Another interpretation: water accumulates on top of walls """
+    """ O(3*n) = O(n) """
+    left = walls.copy()
+    for i in range(1, len(walls)):
+        left[i] = max(left[i-1], left[i])
+    
+    right = walls.copy()
+    for i in range(len(walls)-2, -1, -1):
+        right[i] = max(right[i+1], right[i])
+    
+    water = [0]
+    for i in range(1, len(walls)):
+        water.append(water[i-1] + min(left[i], right[i]) - walls[i])
+    print(water)
+
 # ---------------------------------------------------------
+"""
+Answer: 15
+      |     |
+  |   |   | |
+|_|_|_|_|_|_|_|
+
+Answer: 4
+   |  |
+ | | ||
+||||||||
+"""
 walls = [1, 2, 1, 3, 1, 2, 3, 1]
 walls_and_water_1(walls)
 walls_and_water_2(walls)
+walls_and_water_3(walls)
+print()
+
+"""
+Answer: 15
+..........|
+|         |
+|     |   |
+|_._._|_._|
+
+Answer: 10
+.....|
+|    |
+|  | |
+|__|_|
+"""
+walls = [3, 0, 0, 2, 0, 4]
+walls_and_water_1(walls)
+walls_and_water_2(walls)
+walls_and_water_3(walls)
+print()
