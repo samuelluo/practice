71fa7991c5b2c8f245464c8aa7aa6f276de6b7b8
Samuel Luo <samuel.richard.luo@gmail.com>
2018-08-18 20:42:16 -0400

    Fixed rand5_rand7 in Python.

--- rand5_rand7/rand5_rand7.py
+++ rand5_rand7/rand5_rand7.py
@@ -14,18 +14,30 @@ counts = [0 for i in range(10)]
 for i in range(100000):
     counts[rand5()] += 1
 ratios = [i/sum(counts) for i in counts]
-print(ratios)
-
+# print(ratios)
 
 # rand5   = [1,  2,  3,  4,  5]
-# rand5*5 = [5, 10, 15, 20, 25]
-# rand5*5 + rand5 = [6, 7, ..., 25, 26]
-# rand5*5 + rand5 - 5 = [1, 2, ..., 20, 21]
-# (rand5*5 + rand5 - 5)%7 = [0, 1, 2, 3, 4, 5, 6]
-# (rand5*5 + rand5 - 5)%7 + 1 = [1, 2, 3, 4, 5, 6, 7]
-rand7 = lambda: (rand5()*5 + rand5() - 5)%7 + 1
-counts = [0 for i in range(10)]
+# rand5-1 = [0,  1,  2,  3,  4]
+# (rand5-1)*5 = [0, 5, 10, 15, 20]
+# (rand5-1)*5 + rand5 = [1, 2, ..., 24, 25]
+# (rand5-1)*5 + rand5 - 1 = [0, 1, ..., 23, 24]
+# num -> [0, 1, ..., 20]
+# num%7 = [0, 1, ..., 6]
+# num%7 + 1 = [1, 2, 3, 4, 5, 6, 7]
+def rand7():
+    num = (rand5()-1)*5 + rand5() - 1
+    if num <= 20:
+        return num%7 + 1
+    else:
+        return rand7()
+counts = [0 for i in range(8)]
 for i in range(100000):
     counts[rand7()] += 1
 ratios = [i/sum(counts) for i in counts]
 print(ratios)
+
+
+# ?                           = [7, 8, ..., 33, 34]
+# ?                           = [0, 1, ..., 26, 27]
+# (rand5*5 + rand5 - 5)%7     = [0, 1, 2, 3, 4, 5, 6]
+# (rand5*5 + rand5 - 5)%7 + 1 = [1, 2, 3, 4, 5, 6, 7]
